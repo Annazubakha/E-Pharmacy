1163f074127ab29ded17bdf81bf08d535420c2c4
Annazubakha <korepovaanna@gmail.com>
2024-08-04 18:39:52 +0200

    add initial settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,68 @@
 import type { Config } from "tailwindcss";
-
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      "sm-max": { max: "374.99px" },
+      md: "768px",
+      lg: "1440px",
+    },
+    fontFamily: {
+      inter: [
+        "Inter",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ],
+    },
+    extend: {
+      colors: {
+        "color-bg": "#f7f7f7",
+        "my-green": "#59B17A",
+        "green-dark": "#3F945F",
+        "green-light": "#F7F8FA",
+        "green-10": "#59B17A",
+        "my-black": "#1D1E21",
+        "gris-dark": "#93939A",
+        "my-yellow": "#FFC531",
+        "my-red": "#E85050",
+      },
+    },
   },
-  plugins: [],
+  corePlugins: {
+    container: false,
+  },
+  plugins: [
+    ({ addComponents }) => {
+      addComponents({
+        ".container": {
+          minWidth: "320px",
+          maxWidth: "375px",
+          marginLeft: "auto",
+          marginRight: "auto",
+          paddingLeft: "20px",
+          paddingRight: "20px",
+          "@screen md": {
+            paddingLeft: "32px",
+            paddingRight: "32px",
+            maxWidth: "768px",
+          },
+          "@screen lg": {
+            paddingLeft: "128px",
+            paddingRight: "128px",
+            maxWidth: "1440px",
+          },
+        },
+      });
+    },
+  ],
 } satisfies Config;
